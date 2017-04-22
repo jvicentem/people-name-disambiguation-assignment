95d97f5e62d30125d14a145ef5a604cf43425ce2
Raquel López <r.lopezl.2016@alumnos.urjc.es>
2017-04-22 18:19:07 +0200

    Update PNDBasicClustering.py
    Final file. It contains all modificantions and code comments

--- PNDBasicClustering.py
+++ PNDBasicClustering.py
@@ -1,55 +1,15 @@
-import re, pprint, os, numpy
+import re, os, numpy
 import nltk
-from sklearn.metrics.cluster import *
 from sklearn.cluster import AgglomerativeClustering
-from nltk.cluster import GAAClusterer
 from sklearn.metrics.cluster import adjusted_rand_score
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet as wn
-from nltk import pos_tag
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import RegexpTokenizer
-
-# WordNet only cares about 5 parts of speech.
-# The other parts of speech will be tagged as nouns.
-
-part = {
-    'N' : 'n',
-    'V' : 'v',
-    'J' : 'a',
-    'S' : 's',
-    'R' : 'r'
-}
-
-wnl = WordNetLemmatizer()
-
-def convert_tag(penn_tag):
-    '''
-    convert_tag() accepts the first letter of a Penn part-of-speech tag,
-    then uses a dict lookup to convert it to the appropriate WordNet tag.
-    '''
-    if penn_tag in part.keys():
-        return part[penn_tag]
-    else:
-        # other parts of speech will be tagged as nouns
-        return 'n'
+from nltk import ngrams
+from nltk.tag.stanford import StanfordNERTagger
+from nltk.internals import find_jars_within_path, config_java
 
 
-def tag_and_lem(element):
-    '''
-    tag_and_lem() accepts a string, tokenizes, tags, converts tags,
-    lemmatizes, and returns a string
-    '''
-    # list of tuples [('token', 'tag'), ('token2', 'tag2')...]
-    sent = pos_tag(word_tokenize(element)) # must tag in context
-    return ' '.join([wnl.lemmatize(sent[k][0], convert_tag(sent[k][1][0]))
-                    for k in range(len(sent))])
+#http://stackoverflow.com/a/28327086 -> para solucionar error "list object is not callable", se ha de modificar el archivo nltk/cluster/util.py
 
-#cache de las stopwrods para mejorar rendimiento
-stops = set(stopwords.words('english'))
-
+# Read all documents contained into Thomas Baker directory
 def read_file(file):
     myfile = open(file,"r")
     data = ""
@@ -59,26 +19,25 @@ def read_file(file):
     myfile.close
     return data
 
+# Function used to separate into words of the diferent documents
 def cluster_texts(texts, clustersNumber, distance):
-
     #Load the list of texts into a TextCollection object.
     collection = nltk.TextCollection(texts)
     print("Created a collection of", len(collection), "terms.")
 
-    #get a list of unique terms
-    unique_terms = list(set(collection))
+    # Gget a list of unique terms
+    unique_terms = list(set(collection)) #NUEVO. Para lemmatizaci�n comentar esta l�nea y descomentar la de abajo
+    #unique_terms = collection # NUEVO. S�lo descomentar para lematizaci�n
+
     print("Unique terms found: ", len(unique_terms))
 
     ### And here we actually call the function and create our array of vectors.
-    vectors = [numpy.array(tfidf(f,unique_terms, collection)) for f in texts]
-
+    vectors = [numpy.array(TF_IDF(f,unique_terms, collection)) for f in texts] # NUEVO
     print("Vectors created.")
-    # initialize the clusterer
-    clusterer = GAAClusterer(clustersNumber)
-    clusters = clusterer.cluster(vectors, True)
-    #clusterer = AgglomerativeClustering(n_clusters=clustersNumber,
-    #                                  linkage="average", affinity=distanceFunction)
-    #clusters = clusterer.fit_predict(vectors)
+
+    # Initialize the clusterer -> classify the words into groups
+    clusterer = AgglomerativeClustering(n_clusters=clustersNumber, linkage="average", affinity=distanceFunction)
+    clusters = clusterer.fit_predict(vectors)
 
     return clusters
 
@@ -91,65 +50,102 @@ def TF(document, unique_terms, collection):
         word_tf.append(collection.tf(word, document))
     return word_tf
 
+def IDF(document, unique_terms, collection):
+    word_idf = []
+    for word in unique_terms:
+        word_idf.append(collection.idf(word))
+    return word_idf
 
-#Mejora 1 - cambio del TF por TF-IDF    puntc : -0.14   a  0.16
+# we use that document in the analysis, it presents the best score in all the test performed
+def TF_IDF(document, unique_terms, collection):
+    word_tfidf = []
+    for word in unique_terms:
+        word_tfidf.append(collection.tf_idf(word, document))
+    return word_tfidf
 
-def tfidf(document, unique_terms, collection):
-	word_tfidf = []
-	for word in unique_terms:
-            if re.match('\w+', word):
-    		        word_tfidf.append(collection.tf_idf(word, document))
-	return word_tfidf
 
 if __name__ == "__main__":
-    folder = "Thomas_Baker"
+    folder = "Thomas_Baker" # Call to the folder which contains the different texts
     # Empty list to hold text documents.
     texts = []
 
-    listing = os.listdir(folder)
+    listing = os.listdir(folder) # create a dictionay with all the .txt
+    listing.sort() # to fix indexes, we use the function sort -> we receive always the same result, not random
+
+
+    # We use the stanford named entity relation package to text mining, which contains classifiers to analyze and compare the words
+
+    #stanfordner_dir = '/home/jose/Descargas/stanford-ner-2016-10-31/' #NUEVO
+    stanfordner_dir = './stanford-ner-2016-10-31/'
+    ner_jarfile = stanfordner_dir + 'stanford-ner.jar'
+    ner_modelfile = stanfordner_dir + 'classifiers/english.muc.7class.distsim.crf.ser.gz' #probar con otros
+    st = StanfordNERTagger(ner_modelfile,ner_jarfile)
+
+
+    # read all files contained into listing, and tokenize the words. suddenly, filter the tokens which contains the condition
     for file in listing:
         if file.endswith(".txt"):
             url = folder+"/"+file
             f = open(url,encoding="latin-1");
-            raw = f.read().lower()
+            raw = f.read()
             f.close()
-            #tokens = nltk.word_tokenize(raw)
-            tokenizer = RegexpTokenizer(r'\w+')
-            tokens = tokenizer.tokenize(raw.lower())
 
-            text = nltk.Text(tokens)
+            tokens = nltk.word_tokenize(raw) 
+            #tokens = nltk.word_tokenize(raw.lower()) # NUEVO no se puede usar con EN NER
+            #tokens = wordpunct_tokenize(raw.lower()) #NUEVO no se puede usar con EN NER
 
+            #stop = set(stopwords.words('english')) # NUEVO
+            # filter_tokens = []
+            # for token in tokens:
+            #      if token not in string.punctuation and token not in stop:
+            #          filter_tokens.append(token) 
+            # tokens = filter_tokens 
 
-            #Mejora 2 :Eliminar stopwords: punct: 0.16 a 0.54
-            text = [i for i in text if i not in stops]
+            filter_tokens = []
+            for token in tokens:
+                if re.search('[a-zA-Z0-9]', token):
+                    filter_tokens.append(token)
+            tokens = filter_tokens
 
+            named_entities = []
+            tagged_words = st.tag(tokens)
 
-            #Mejora 4: Lemmatiser   empeora a 0.33
-            #text = [tag_and_lem(i) for i in text if len(i)>2]
-                        #Mejora 3: Stemmer  de 0.54 a 0.55
-            #stemmer = SnowballStemmer("english")
-            #text = [stemmer.stem(i) for i in text]
+            # set a tag for all tokens to name the different entities
+            for tagged_word in tagged_words:
+                if tagged_word[1] != 'O':
+                    named_entities.append(tagged_word[0])
 
-            print("___sinonimoooos")
-            #mejora 5 : sinonimos
-            #for word in text:
-            #	wordNetSynset = wn.synsets(word)
-            #	for synSet in wordNetSynset:
-            #		for synWords in synSet.lemma_names():
-            #			text.append(synWords[0])
+            # classify the different named entities into 3 ngrams (groups)
+            n = 3
+            ngramss = list(set(ngrams(named_entities, n)))
+                         
+            text = nltk.Text(ngramss) #/NUEVO
+            #text = nltk.Text(tokens)
             texts.append(text)
+
+
 
     print("Prepared ", len(texts), " documents...")
     print("They can be accessed using texts[0] - texts[" + str(len(texts)-1) + "]")
 
-    distanceFunction ="cosine"
-    #distanceFunction = "euclidean"  #0.744623655914
+    # A distance function is used to locate and compare the ambiguation (similarity) of the tokens
+    distanceFunction  = "braycurtis"   #  BEST RAND SCORE: 0.893854748603352
+    #distanceFunction = "cosine"       <- BEST RAND SCORE: 0.893854748603352
+    #distanceFunction = "dice"         <- 0.8038540949759119
+    #distanceFunction = "manhattan"    <- 0.2261521972132905
+    #distanceFunction = "euclidean"    <- 0.0836012861736335
+    #distanceFunction = "jaccard"      <- 0.06323687031082535
+    #distanceFunction = "correlation"  <- 0.011312217194570151
+    #distanceFunction = "hamming"      <- -0.058949624866023516
 
+
+    # this array contains the results of our analysis given a distance function -> identify which thomas baker is named in each documents
     test = cluster_texts(texts,4,distanceFunction)
+
     print("test: ", test)
-    # Gold Standard
+    # Gold Standard -> create an array with the default data (reference)
     reference =[0, 1, 2, 0, 0, 0, 3, 0, 0, 0, 2, 0, 3, 3, 0, 1, 2, 0, 1]
     print("reference: ", reference)
 
-    # Evaluation
+    # Evaluation -> print the final result of the disambiguation
     print("rand_score: ", adjusted_rand_score(reference,test))
